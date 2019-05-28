4b40202df3fc15891084e07b2bdf7e746b3adbe4
DaveOuds <daveouds@gmail.com>
2019-05-28 17:47:22 +0200

    message test

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const SocketServer = require('ws').Server;
 const path = require('path');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const INDEX = path.join(__dirname, 'index.html');
 
 const _ = require('lodash');
@@ -14,55 +14,91 @@ let itemList = []
 let users = []
 
 const server = express()
-    .use((req, res) => res.sendFile(INDEX))
-    .listen(PORT, () => console.log(`Listening on ${PORT}`));
+  .use((req, res) => res.sendFile(INDEX))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const wss = new SocketServer({ server });
 
 wss.on('connection', socket => {
-    socket.on('message', message => {
-        console.log(wss.clients)
-        // Game Master messages
-        if (message.client == "GM") {
-            switch (message.type) {
-                case "start game":
-                        wss.send("start game")
-                    break
-                case "start levels":
-                        wss.send("start levels")
-                    break
-                case "":
-            }
-        } // Player messages 
-        else if (message.client == "player") {
-            switch (message.type) {
-                case "connect":
-                    wss.send("accepted")
-                    break;
+  socket.on('message', message => {
+    console.log(wss.clients)
+    // Game Master messages
+    if (message.client == "GM") {
+      switch (message.type) {
+        case "start game":
+          wss.send("start game")
+          break
+        case "start levels":
+          wss.send("start levels")
+          break
+        case "":
+      }
+    } // Player messages 
+    else if (message.client == "player") {
+      switch (message.type) {
+        case "connect":
+          wss.send("accepted")
+          break;
 
-                case "items":
-                    _.forEach(message.data, (item) => {
-                        if (_.includes(itemList, item)) {
-                            itemList[item]++
-                        } else {
-                            itemList[item] = 1
-                        }
-                    })
-                    break;
+        case "items":
+          _.forEach(message.data, (item) => {
+            if (_.includes(itemList, item)) {
+              itemList[item]++
+            } else {
+              itemList[item] = 1
             }
+          })
+          break;
+      }
 
-        } else {
-            socket.destroy()
-        }
-    });
+    } else {
+      socket.destroy()
+    }
+  });
 });
 wss.on('connection', (ws) => {
-    console.log('Client connected');
-    ws.on('close', () => console.log('Client disconnected'));
+  console.log('Client connected');
+
+  ws.on('message', (message) => {
+    console.log(message)
+    
+      // Game Master messages
+      if(message.client == "GM") {
+    switch (message.type) {
+      case "start game":
+        wss.send("start game")
+        break
+      case "start levels":
+        wss.send("start levels")
+        break
+      case "":
+    }
+  } // Player messages 
+      else if (message.client == "player") {
+    switch (message.type) {
+      case "connect":
+        wss.send("accepted")
+        break;
+
+      case "items":
+        _.forEach(message.data, (item) => {
+          if (_.includes(itemList, item)) {
+            itemList[item]++
+          } else {
+            itemList[item] = 1
+          }
+        })
+        break;
+    }
+  }
+})
+
+ws.on('close', () => console.log('Client disconnected'));
 });
 
 setInterval(() => {
-    wss.clients.forEach((client) => {
-        client.send(new Date().toTimeString());
-    });
+  wss.clients.forEach((client) => {
+    console.log(JSON.stringify(client))
+    client.send(new Date().toTimeString());
+  });
 }, 1000);
