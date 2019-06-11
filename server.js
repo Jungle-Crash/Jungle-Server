a1d526d062e2d2d33d82c85b15b67ca5eeec1734
DaveOuds <daveouds@gmail.com>
2019-06-11 16:38:01 +0200

    player login test5

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ wss.on('connection', (ws) => {
       console.log("Player")
       switch (msg.type) {
         case "login":
-          const client = request.accept('echo-protocol', request.origin);
+          const client = ws.accept('echo-protocol', request.origin);
           playerList[msg.username] = client
           ws.send("accepted")
           break
